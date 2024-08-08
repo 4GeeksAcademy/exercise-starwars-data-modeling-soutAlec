a1ad8a34bdd96e813f9b9e20fa3850048cce448f
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-08-08 11:27:16 +0000

    data modeling

--- src/models.py
+++ src/models.py
@@ -1,32 +1,63 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from datetime import datetime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+    contraseña = Column(String(250), nullable=False)
+    fecha_subscripcion = Column(DateTime, default=datetime.utcnow)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    planetas_favoritos = relationship('Favorito', foreign_keys='Favorito.usuario_id', back_populates='usuario')
+    personajes_favoritos = relationship('Favorito', foreign_keys='Favorito.usuario_id', back_populates='usuario')
+
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(250), nullable=False)
+    clima = Column(String(250))
+    terreno = Column(String(250))
 
-    def to_dict(self):
-        return {}
+    
+    favoritos = relationship('Favorito', foreign_keys='Favorito.planeta_id', back_populates='planeta')
 
-## Draw from SQLAlchemy base
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    altura = Column(Integer)
+    peso = Column(Integer)
+    color_cabello = Column(String(250))
+    color_piel = Column(String(250))
+    color_ojo = Column(String(250))
+
+
+    favoritos = relationship('Favorito', foreign_keys='Favorito.personaje_id', back_populates='personaje')
+
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    planeta_id = Column(Integer, ForeignKey('planeta.id'), nullable=True)
+    personaje_id = Column(Integer, ForeignKey('personaje.id'), nullable=True)
+
+    usuario = relationship('Usuario', back_populates='planetas_favoritos')
+    planeta = relationship('Planeta', back_populates='favoritos')
+    personaje = relationship('Personaje', back_populates='favoritos')
+
 render_er(Base, 'diagram.png')
+
